3dd37892d5037789c89709becda8b5b5026ccb59
Sadia Batool <sadia110@my.yorku.ca>
2020-07-06 03:30:30 -0400

    finished functions

---
+++ script.js
@@ -0,0 +1,107 @@
+//  for next button
+var nextButton = document.getElementById('btnNext');
+//  for quiz box
+var quizbox = document.getElementById('questionBox') 
+
+
+// for options 
+
+var opt1 = document.getElementById('opt1')
+var opt2 = document.getElementById('opt2')
+var opt3 = document.getElementById('opt3')
+var opt4 = document.getElementById('opt4')
+
+// / for list
+var ul = document.getElementById('ul') 
+
+
+// questions query
+var app={
+        questions:[
+            {
+                q:'Who is the caption of the Leafs?',
+                options: ['Matthews', 'Marner', '`Nylander', 'Tavares'],
+                answer:4
+            },
+            {
+                q:'When did the Raptors won the NBA championship',
+                options: ['1999', '2018', '2019', 'Never'],
+                answer:3
+            }     
+            
+          
+        ],
+
+
+
+        index:0,
+        load:function(){
+            if(this.index<=this.questions.length-1){
+                quizbox.innerHTML=this.index+1 + ". " +this.questions[this.index].q;
+                opt1.innerHTML=this.questions[this.index].options[0];
+                opt2.innerHTML=this.questions[this.index].options[1];
+                opt3.innerHTML=this.questions[this.index].options[2];
+                opt4.innerHTML=this.questions[this.index].options[3];
+            }
+            else {
+                quizbox.innerHTML="End of Quiz";
+                ul.style.display="none";
+                nextButton.style.display="none";
+            }
+        },
+        next: function(){
+            this.index++;
+            this.load();
+        },
+        check: function(ele){
+            var id=ele.id.split('');
+            if(id[id.length-1]==this.questions[this.index].answer){
+                this.score++;
+                ele.className="correct";
+                this.scoreCard();
+            }
+            else{
+                ele.className="wrong";
+            }
+        },
+
+        //  
+        preventClick:function(){
+            for(let i=0; i<ul.children.length; i++){
+                ul.children[i].style.pointerEvents="none";
+            }
+        },
+        allowClick:function(){
+            for(let i=0; i<ul.children.length; i++){
+                ul.children[i].style.pointerEvents="auto";
+                ul.children[i].className=''
+            }
+        },
+        score:0,
+        scoreCard:function(){
+            scoreCard.innerHTML=this.questions.length + "/" + this.score;
+        }
+} 
+
+
+
+
+
+
+
+
+
+//  Event listeners NEED TO FIXXXX
+
+window.load=app.load();
+
+function button(ele){
+    app.check(ele);
+    app.preventClick();
+}
+
+function next(){
+    app.next();
+    app.allowClick();
+} 
+
